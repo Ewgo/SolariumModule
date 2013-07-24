09c336d9eb141c09fd9211db515475b1d2d94170
Jonathan CHAMPION <champijo@gmail.com>
2013-07-24 11:27:33 +0200

    Initialize module

---
+++ Module.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SmileSolarium;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface,
+    ServiceProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'invokables' => array(
+            ),
+            'factories' => array(
+            ),
+        );
+    }
+}
